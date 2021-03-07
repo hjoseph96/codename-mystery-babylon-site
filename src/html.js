00071a973a6c6b1d1dacf5c06e7cbc2642e8b953
Herbert Joseph <techniquea2z@gmail.com>
2021-03-07 16:17:58 -0500

    move to using identity

--- src/html.js
+++ src/html.js
@@ -17,6 +17,7 @@ export default function HTML(props) {
           href="opensearch.xml"
         />
         {props.headComponents}
+         <script src="https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js"></script>
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
