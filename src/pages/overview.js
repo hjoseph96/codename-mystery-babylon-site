3ad821e3fd18a15d5d43a5c5030c109640062e74
Herbert Joseph <techniquea2z@gmail.com>
2021-03-08 19:49:33 -0500

    Add scroll and header

--- src/pages/overview.js
+++ src/pages/overview.js
@@ -2,26 +2,38 @@ import React from "react";
 import loadable from '@loadable/component';
 import ReactFullpage from '@fullpage/react-fullpage';
 
+import '../scss/overview.scss'
+
 import Navbar from '../components/Navbar';
 const PurpleSmoke = loadable(() => import('../components/PurpleSmoke'));
 
 
 class Overview extends React.Component {
   render() {
-    const Fullpage = () => (
+    const FullPage = () => (
       <ReactFullpage
         //fullpage options
         licenseKey = {'YOUR_KEY_HERE'}
-        scrollingSpeed = {1000} /* Options here */
+        scrollingSpeed = {1000}
+        navigation
 
         render={({ state, fullpageApi }) => {
           return (
             <ReactFullpage.Wrapper>
               <div className="section">
-                <p>Section 1 (welcome to fullpage.js)</p>
-                <button onClick={() => fullpageApi.moveSectionDown()}>
-                  Click me to move down
-                </button>
+                <main>
+                  <h2>Infiltrate The Ranks of The Wicked</h2>
+
+                  <div className="scroll">
+                    <div className="text-container">
+                      <p>
+                        The rich and powerful of the land operate secret
+                        organizations, unbeknownst to most. Their greed and depravity
+                        knows no bounds, as they covertly wrest control of all the nations.
+                      </p>
+                    </div>
+                  </div>
+                </main>
               </div>
               <div className="section">
                 <p>Section 2</p>
@@ -31,13 +43,13 @@ class Overview extends React.Component {
         }}
       />
     );
-    
+
     return (
       <div className="overview">
         <Navbar />
         <PurpleSmoke />
 
-
+        <FullPage />
       </div>
     )
   }
