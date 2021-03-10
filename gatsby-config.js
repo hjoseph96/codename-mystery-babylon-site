374089a057d91b11f5a421f19f3f20a32f565832
Herbert Joseph <techniquea2z@gmail.com>
2021-03-10 18:10:15 -0500

    Add robots.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-fontawesome-css`,
+    'gatsby-plugin-robots-txt',
     {
       resolve: "gatsby-plugin-local-search",
       options: {
