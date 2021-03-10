99dc18673b2fb24710f708052f53a1d070499eda
Herbert Joseph <techniquea2z@gmail.com>
2021-03-10 18:19:52 -0500

    Add sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-fontawesome-css`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
